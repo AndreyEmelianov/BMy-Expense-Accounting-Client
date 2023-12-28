b0861d2aa645719fdf34f7e18daabeb11f7337aa
Andrey Emelianov <621077@mail.ru>
2023-12-28 15:49:41 +0300

    [feat(router)]: подключил react-router-dom, настроил роуты, реализовал разметку и роутинг для компонента Header и добавил его на страницу Layout, реализовал страницу ErrorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,6 +1,16 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+import NotFoundImg from '../assets/pg-not-found.png';
 
 const ErrorPage: FC = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="min-h-screen bg-slate-900 font-roboto text-white flex justify-center items-center flex-col gap-10">
+      <img src={NotFoundImg} alt="page not found" />
+      <Link to={'/'} className="bg-sky-500 rounded-md px-6 py-2 hover:bg-sky-600">
+        Back
+      </Link>
+    </div>
+  );
 };
 export default ErrorPage;
